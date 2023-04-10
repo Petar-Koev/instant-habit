3366f27297fdf5a0cdadaf591b35ba6983d2205c
Petar-Koev <petar.koev94@gmail.com>
2023-04-10 18:59:50 +0200

    unitTest+

--- BackEnd/InstantHabit/InstantHabit.Tests.Unit/HabitsControllerTests.cs
+++ BackEnd/InstantHabit/InstantHabit.Tests.Unit/HabitsControllerTests.cs
@@ -14,7 +14,7 @@ namespace InstantHabit.Tests.Unit
 {
     public class HabitsControllerTests
     {
-        /*
+        
         private readonly Mock<IHabitsService> _habitsServiceMock;
         private readonly HabitsController _habitsController;
 
@@ -22,10 +22,8 @@ namespace InstantHabit.Tests.Unit
         {
             _habitsServiceMock = new Mock<IHabitsService>();
             _habitsController = new HabitsController(_habitsServiceMock.Object);
-
         }
-        */
-
+        
         [Fact]
         public async Task AddHabit_ReturnsAddHabitResponse_WithSucceededTrue_And_WithoutErrors_WhenMatchCheckerEqualsNoMatch()
         {
@@ -34,16 +32,12 @@ namespace InstantHabit.Tests.Unit
             var addHabitRequest = new AddHabitRequest();
             addHabitRequest.Name = "Gosho";
 
-            var habitsServiceMock = new Mock<IHabitsService>();
-
-            habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("No match");
-
-            var habitsController = new HabitsController(habitsServiceMock.Object);
+            _habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("No match");
            
 
             // When
 
-            var addHabitResponse = await habitsController.AddHabit(addHabitRequest);
+            var addHabitResponse = await _habitsController.AddHabit(addHabitRequest);
 
             // Then
 
@@ -60,17 +54,12 @@ namespace InstantHabit.Tests.Unit
             var addHabitRequest = new AddHabitRequest();
             addHabitRequest.Name = "WorkOut";
 
-            var habitsServiceMock = new Mock<IHabitsService>();
-
-            habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("No match");
-            habitsServiceMock.Setup((m) => m.CreateNewHabit(addHabitRequest)).Throws(new Exception("bla"));
-
-            var habitsController = new HabitsController(habitsServiceMock.Object);
-
+            _habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("No match");
+            _habitsServiceMock.Setup((m) => m.CreateNewHabit(addHabitRequest)).Throws(new Exception("bla"));
 
             // When
 
-            var addHabitResponse = await habitsController.AddHabit(addHabitRequest);
+            var addHabitResponse = await _habitsController.AddHabit(addHabitRequest);
 
             // Then
 
@@ -87,16 +76,11 @@ namespace InstantHabit.Tests.Unit
             var addHabitRequest = new AddHabitRequest();
             addHabitRequest.Name = "WorkOut";
 
-            var habitsServiceMock = new Mock<IHabitsService>();
-
-            habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("Match");
-
-            var habitsController = new HabitsController(habitsServiceMock.Object);
-
+            _habitsServiceMock.Setup((m) => m.MatchChecker(addHabitRequest.Name)).Returns("Match");
 
             // When
 
-            var addHabitResponse = await habitsController.AddHabit(addHabitRequest);
+            var addHabitResponse = await _habitsController.AddHabit(addHabitRequest);
 
             // Then
 
